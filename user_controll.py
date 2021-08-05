ba8318387ec9ad69db7dc1d5cc33a4f83e67f848
DjLawRee <80518870+DjLawRee@users.noreply.github.com>
2021-08-05 15:00:33 +0300

    starting on propper login

--- user_controll.py
+++ user_controll.py
@@ -3,7 +3,9 @@ import mysql.connector
 db=mysql.connector.connect(user='root',password='MyNewPass',
                         host='127.0.0.1',
                         database='instaclone')
-print("Conection id: ", db.connection_id)
+cursor=db.cursor()
+
+
 
 
 
