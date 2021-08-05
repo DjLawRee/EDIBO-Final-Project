08d0387322d66615da150db44813e534fde3355d
DjLawRee <80518870+DjLawRee@users.noreply.github.com>
2021-08-05 09:32:53 +0300

    Initial Commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from werkzeug.security import generate_password_hash , check_password_hash
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+
+
+app=Flask(__name__)
+
+@app.route("/")
+def home():
+    return "hello Flask!"
+
+
