d1b73f07edf6903cc51a0a2fb4dc052b9843378c
Edas <edas.dev@gmail.com>
2021-08-05 10:44:53 +0300

    Testing flask

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
-from flask import Flask
+from flask import Flask, flash, redirect, render_template, request, session
+from tempfile import mkdtemp
 from werkzeug.security import generate_password_hash , check_password_hash
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 
 
 app=Flask(__name__)
 
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
 @app.route("/")
 def home():
     return "Here we Go testing!"
 
+
+@app.route("/test")
+def test():
+    return render_template("test.html")
 
