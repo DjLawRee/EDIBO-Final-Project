35ff7d0f28471d392c2424d4221a94f7430ac23d
DjLawRee <80518870+DjLawRee@users.noreply.github.com>
2021-08-07 14:05:03 +0300

    what

--- user_controll.py
+++ user_controll.py
@@ -8,18 +8,18 @@ db=mysql.connector.connect(user='root',password='MyNewPass',
 
 
 def check_if_user_exists(user_name):
-    cursor=db.cursor()
+    cursor=db.cursor(dictionary=True)
     query=("SELECT Id FROM users WHERE user_name = %s ")
     cursor.execute(query,(user_name,))
     result = cursor.fetchone()
-    if result[0]:
+    if result:
         return True
     else:
         return False
     
 
 def get_user_data(user_name):
-    cursor=db.cursor()
+    cursor=db.cursor(dictionary=True)
     query=("SELECT * FROM users WHERE user_name = %s ")
     cursor.execute(query,(user_name,))
     user_data = cursor.fetchone()
@@ -27,7 +27,7 @@ def get_user_data(user_name):
     return user_data
 
 def get_user_password(user_name):
-    cursor=db.cursor()
+    cursor=db.cursor(dictionary=True)
     query=("SELECT password FROM users WHERE user_name = %s ")
     cursor.execute(query,(user_name,))
     result=cursor.fetchone()
@@ -36,7 +36,8 @@ def get_user_password(user_name):
 def register_user(user_name,password):
     cursor=db.cursor()
     query=("INSERT INTO users (user_name,password_hash) VALUES(%s,%s)")
-    cursor.
+    cursor.execute(query,(user_name,),(password,))
+
 
 
 
